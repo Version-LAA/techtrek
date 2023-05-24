4120b0883dcce97179bc3816363ddf534fad3ec0
Latoya Alford <lat.jamison@gmail.com>
2023-05-23 21:28:49 -0400

    styled profile and created partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def my_profile
+    @user = current_user
+    @specialties = Specialty.where(user: @user)
+    @education = Education.where(user: @user)
+    @experiences = Experience.where(user: @user)
+  end
 end
