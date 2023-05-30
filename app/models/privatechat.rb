162610f4c1a14d37ea9f6613155029316a07ffb6
Latoya Alford <lat.jamison@gmail.com>
2023-05-30 13:39:40 -0400

    deleted old model and controller

--- app/models/privatechat.rb
+++
@@ -1,4 +0,0 @@
-class Privatechat < ApplicationRecord
-  has_many :messages
-  belongs_to :user
-end
--- test/controllers/privatechats_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PrivatechatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
