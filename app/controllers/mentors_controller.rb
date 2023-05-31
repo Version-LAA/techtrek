1d9120c780e5469423ea317defb19a7edfe52471
Latoya Alford <lat.jamison@gmail.com>
2023-05-30 21:09:36 -0400

    trying chat feature

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -4,10 +4,18 @@ class MentorsController < ApplicationController
   end
 
   def show
+    @channel = ChatChannel.create
     @mentor = User.find(params[:id])
     @message = Message.new(receiver:@mentor)
     @specialties = Specialty.where(user: @mentor).order(skill_level: :desc)
     @education = Education.where(user: @mentor)
     @experiences = Experience.where(user: @mentor)
+  end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
