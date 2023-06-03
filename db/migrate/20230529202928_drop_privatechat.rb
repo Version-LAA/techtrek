5bbefa43352c6f7471ee439d07bfcda2772dfdf1
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-06-03 10:35:09 -0400

    fixed error on zoom

--- db/migrate/20230529202928_drop_privatechat.rb
+++
@@ -1,5 +0,0 @@
-class DropPrivatechat < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :privatechats, force: :cascade
-  end
-end
--- db/migrate/20230531015632_drop_message_channel.rb
+++
@@ -1,5 +0,0 @@
-class DropMessageChannel < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :message_channels
-  end
-end
