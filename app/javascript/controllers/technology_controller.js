38339ce1ed1462063c05df745193e7d5a720c4fa
Latoya Alford <lat.jamison@gmail.com>
2023-06-07 20:01:58 -0400

    light styling

--- app/javascript/controllers/technology_controller.js
+++ app/javascript/controllers/technology_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="technology"
 export default class extends Controller {
+  static targets = ['tech']
   connect() {
     console.log("hello from technology page");
+  }
+
+  selecttech(){
+    // console.dir(this.techTarget);
+    console.log("selected");
+
   }
 }
