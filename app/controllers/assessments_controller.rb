90ce96819d813a3f6c4f895b77dbf91ba33e5836
Latoya Alford <lat.jamison@gmail.com>
2023-06-10 13:34:38 -0400

    defined controller action to show mentors based on assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,6 +1,19 @@
 class AssessmentsController < ApplicationController
   def new
-    raise
+    search = params[:technologies]
+
+    if search.present?
+      sql_subquery = <<~SQL
+        technologies.name ILIKE :search
+      SQL
+      @mentors = User.joins(specialties: [:technology])
+                     .where(technologies: { name: search })
+                     .where("specialties.skill_level > ?", 3)
+                     .uniq
+    else
+      @mentors = User.all
+    end
+
   end
 
   def create
