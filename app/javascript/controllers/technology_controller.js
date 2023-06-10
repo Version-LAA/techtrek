d723c52ee775cfe2b98205a78c830d4780d59e4e
Latoya Alford <lat.jamison@gmail.com>
2023-06-10 09:20:55 -0400

    js parameters fetched

--- app/javascript/controllers/technology_controller.js
+++ app/javascript/controllers/technology_controller.js
@@ -15,5 +15,44 @@ export default class extends Controller {
     } else {
       card.classList.remove("checked");
     }
+
+    const checkedItems = this.getCheckedItems();
+
+    console.log(checkedItems);
   }
+
+  getCheckedItems() {
+    const checkedItems = [];
+
+    this.techTargets.forEach((checkbox) => {
+      if (checkbox.checked) {
+        const params = {
+          name: checkbox.value,
+        };
+        checkedItems.push(params);
+      }
+    });
+
+    return checkedItems;
+  }
+
+  sendCheckedItemsToController(checkedItems) {
+    const url = '/specialties';
+    const method = 'GET';
+    const data = { checkedItems: JSON.stringify(checkedItems) };
+
+    fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(response => {
+
+        console.log(response);
+      });
+  }
+
 }
