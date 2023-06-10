9a2d5a9d52ed23ada546896473016f7976bee888
Latoya Alford <lat.jamison@gmail.com>
2023-06-10 12:05:50 -0400

    created simple form for assessment

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,13 +1,6 @@
 class AssessmentsController < ApplicationController
   def new
-    @assessment = Assessment.new(user: current_user)
-    @question_bank = Question.all
-    @question_bank.each do |ques|
-      @assessment_question = AssessmentQuestion.new(question: ques, assessment: @assessment)
-      @assessment_question.save
-    end
-    @user_assessment = AssessmentQuestion.where(assessment_id:@assessment)
-
+    raise
   end
 
   def create
