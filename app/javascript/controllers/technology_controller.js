0c736032b236afeb22276e2138053f38a151d561
Latoya Alford <lat.jamison@gmail.com>
2023-06-11 18:33:05 -0400

    hide checkbox

--- app/javascript/controllers/technology_controller.js
+++ app/javascript/controllers/technology_controller.js
@@ -5,12 +5,14 @@ export default class extends Controller {
   static targets = ['tech']
   connect() {
     console.log("hello from technology page");
+
   }
 
   toggleParent(event) {
 
    const selectedCard = event.target.closest('.technology-card');
    const checkbox = selectedCard.querySelector('input[type="checkbox"]');
+   console.dir(selectedCard);
 
    console.log( checkbox.checked = !checkbox.checked);
 
