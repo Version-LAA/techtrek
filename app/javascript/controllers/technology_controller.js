6baedc012eb69b3abccae2ceabf0c8797ea4c0c3
Latoya Alford <lat.jamison@gmail.com>
2023-06-11 18:24:01 -0400

    implemented checkbox function

--- app/javascript/controllers/technology_controller.js
+++ app/javascript/controllers/technology_controller.js
@@ -8,51 +8,19 @@ export default class extends Controller {
   }
 
   toggleParent(event) {
-    const checkbox = event.currentTarget;
-    const card = checkbox.parentElement;
-    if (checkbox.checked) {
-      card.classList.add("checked");
-    } else {
-      card.classList.remove("checked");
-    }
 
-    const checkedItems = this.getCheckedItems();
+   const selectedCard = event.target.closest('.technology-card');
+   const checkbox = selectedCard.querySelector('input[type="checkbox"]');
 
-    console.log(checkedItems);
-  }
+   console.log( checkbox.checked = !checkbox.checked);
 
-  getCheckedItems() {
-    const checkedItems = [];
-
-    this.techTargets.forEach((checkbox) => {
-      if (checkbox.checked) {
-        const params = {
-          name: checkbox.value,
-        };
-        checkedItems.push(params);
-      }
-    });
-
-    return checkedItems;
-  }
-
-  sendCheckedItemsToController(checkedItems) {
-    const url = '/specialties';
-    const method = 'GET';
-    const data = { checkedItems: JSON.stringify(checkedItems) };
-
-    fetch(url, {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
-      .then(response => {
-
-        console.log(response);
-      });
-  }
+     if(checkbox.checked){
+      selectedCard.classList.add("checked");
+     } else {
+      checkbox.checked = false;
+      selectedCard.classList.remove("checked");
+      console.log(checkbox.checked);
+     }
+   }
 
 }
