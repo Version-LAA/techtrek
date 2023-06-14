596f852307750a9abf329e4bb37c8efa335954fa
Latoya Alford <lat.jamison@gmail.com>
2023-06-14 08:47:28 -0400

    added the ability to see matches when not logged in

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,17 +1,25 @@
 class AssessmentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new]
   def new
     search = params[:technologies]
 
-    if search.present?
-      @mentors = User.joins(specialties: [:technology])
-                     .where(technologies: { name: search })
-                     .where("specialties.skill_level > ?", 3)
-                     .where.not(users: { id: current_user.id })
-                     .distinct
+    if current_user
+      if search.present?
+        @mentors = User.joins(specialties: [:technology])
+                      .where(technologies: { name: search })
+                      .where("specialties.skill_level > ?", 3)
+                      .where.not(users: { id: current_user.id })
+                      .distinct
 
 
+      else
+        @mentors = User.all
+      end
     else
-      @mentors = User.all
+      @mentors = User.joins(specialties: [:technology])
+                      .where(technologies: { name: search })
+                      .where("specialties.skill_level > ?", 3)
+                      .distinct
     end
 
   end
