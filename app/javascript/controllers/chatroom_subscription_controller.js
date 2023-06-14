e9916e30e6dfd38f0e107473b0b877f5e3aa8822
Latoya Alford <lat.jamison@gmail.com>
2023-06-14 18:49:15 -0400

    added JS for message styling of user vs sender

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,7 +4,7 @@ import { createConsumer } from "@rails/actioncable"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static targets = ["messages"]
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number,currentUserId: Number }
 
   connect() {
     console.log(this.chatroomIdValue);
@@ -22,7 +22,28 @@ export default class extends Controller {
   // private function
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id;
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
   belongs_to :chat_channel
+
 end
